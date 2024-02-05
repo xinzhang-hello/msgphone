8b3547a35919e83970f0ddfb52f9d3c72fd192d5
zhangxin <xinzhang.hi@outlook.com>
2024-02-05 17:04:47 +0800

    Init sms receiver

---
+++ app/src/main/java/com/example/msgphone/SmsListenerService.kt
@@ -0,0 +1,11 @@
+package com.example.msgphone
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SmsListenerService : Service(){
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
